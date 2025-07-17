36b65339c47bf4ce5840243417038132e8d265a7
Armanul Momin <armanul.momin@gmail.com>
2025-07-17 14:09:45 +1000

    first commit

---
+++ RestRitual/RestRitualApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestRitualApp.swift
+//  RestRitual
+//
+//  Created by Arman on 17/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RestRitualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
